43612e14e79b094844e12bc98ac9d6ab317e6185
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-10 19:34:07 +0530

    create login

--- app/routers/user.py
+++ app/routers/user.py
@@ -32,3 +32,22 @@ def create_user(user: user.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+#login user
+@router.post("/login", response_model=token.Token)
+def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    
+    user = db.query(user_model.User).filter(user_model.User.email == user_credentials.username).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Invalid Credentials")
+    
+    if not utils.verify(user_credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Invalid password Credentials")
+    
+    # create a token
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    
+    return {"access_token": access_token, "token_type": "bearer"}
