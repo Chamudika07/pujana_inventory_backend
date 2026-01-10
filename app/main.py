b891cddee493f4e5f64ea91647d4cd278383f370
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-10 22:22:12 +0530

    some changes with alembic

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from fastapi import FastAPI
 from app.models import user as models
-from app.database import engine, test_db_connection
+from app.database import engine
 from app.routers import user , category
 
 app = FastAPI(title="Inventory System")
 
-models.Base.metadata.create_all(bind=engine)
+
 
 app.include_router(user.router)
 app.include_router(category.router)
 
-@app.on_event("startup")
-def startup_event():
-    test_db_connection()
-    models.Base.metadata.create_all(bind=engine)
+
     
     
 @app.get("/")
