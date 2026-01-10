49d7bb89102ac8fa11e2e1c12374e22e22ef0fd9
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-10 19:59:50 +0530

    create schemas for cetogory

---
+++ app/schemas/category.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel 
+from datetime import datetime
+
+class CategoryBase(BaseModel):
+    name: str
+    description: str | None = None
+    
+class CategoryCreate(CategoryBase):
+    pass
+
+class CategoryOut(CategoryBase):
+    id: int
+    name: str
+    description: str | None = None
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
