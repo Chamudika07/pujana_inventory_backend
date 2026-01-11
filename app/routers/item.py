a47ce1c3a455e378663028162347321b09c9f045
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-11 23:32:49 +0530

    creat item

--- app/routers/item.py
+++ app/routers/item.py
@@ -6,6 +6,7 @@ from app.database import get_db
 from app.schemas import item
 from sqlalchemy import func
 
+
 router = APIRouter(
     prefix="/items",
     tags=['items']
@@ -20,7 +21,26 @@ def create_item(item : item.ItemCreate , db: Session = Depends(get_db),
     # check if category name already exists
     existing_item = (db.query(
         item_model.Item)
-            .filter(func.lower(item_model.Item.name) == item.name.lower()).first() )
+            .filter(func.lower(item_model.Item.model_number) == item.model_number.lower()).first())
+    
+    if existing_item:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f"Item with name {item.name} already exists")
+        
+    new_item = item_model.Item(**item.dict())
+    
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    
+    return new_item
+    
+    
+    
+    
+    
+    
+    
     
     
     
