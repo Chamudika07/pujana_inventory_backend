e41ef76a66a36462103cfbef797fd36d84e91ae1
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-11 23:33:44 +0530

    conect to item in main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.models import user as models
 from app.database import engine
-from app.routers import user , category
+from app.routers import user , category , item
 
 app = FastAPI(title="Inventory System")
 
@@ -9,8 +9,7 @@ app = FastAPI(title="Inventory System")
 
 app.include_router(user.router)
 app.include_router(category.router)
-
-
+app.include_router(item.router)
     
     
 @app.get("/")
