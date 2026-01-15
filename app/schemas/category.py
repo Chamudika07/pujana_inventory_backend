06e2ac3423ab1636651c69c26abbf239067513b3
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-15 07:23:34 +0530

    change schemas

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -9,10 +9,6 @@ class CategoryBase(BaseModel):
 class CategoryCreate(CategoryBase):
     pass
 
-class CategoryUpdate(BaseModel):
-    name : Optional[str] = None
-    description : Optional[str] = None
-
 class CategoryOut(CategoryBase):
     id: int
     name: str
