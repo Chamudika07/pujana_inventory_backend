60c733381191c8a7ccd8b12105f48c94b2d58fa0
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2026-01-18 09:21:11 +0530

    re create inventory model

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -11,11 +11,12 @@ class InventoryTransaction(Base):
     __tablename__ = "inventory_transactions"
     
     id = Column(Integer , primary_key = True , nullable = False)
+    bill_id = Column(String , ForeignKey("bills.bill_id" , ondelete = "CASCADE") , nullable = False)
     item_id = Column(Integer , ForeignKey("items.id" , ondelete = "CASCADE" ) , nullable = False)
     transaction_type = Column(String , nullable = False) #buy/sell
     quantity = Column(Integer , nullable = False , server_default="0")
-    buying_price = Column(Numeric(10, 2) , nullable = False)
-    selling_price = Column(Numeric(10, 2) , nullable = False)
+    price = Column(Numeric(10, 2) , nullable = False)
     created_at = Column(TIMESTAMP , nullable = False , server_default = text('now()')) 
     
     items = relationship("Item" , back_populates= "inventory_transaction")
+    bill = relationship("Bill" , back_populates = "inventory_transactions")
